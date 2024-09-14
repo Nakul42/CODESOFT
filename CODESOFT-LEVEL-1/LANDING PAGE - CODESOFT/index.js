f8e855423d26da6112a6fb022f5f00cd5c2099ae
Nakul42 <103737652+Nakul42@users.noreply.github.com>
2024-09-14 23:56:37 +0530

    Add files via upload

---
+++ CODESOFT-LEVEL-1/LANDING PAGE - CODESOFT/index.js
@@ -0,0 +1,10 @@
+// window.addEventListener('scroll', function() {
+//   var box = document.querySelector('.box');
+//   var boxPosition = box.getBoundingClientRect().top;
+//   var windowHeight = window.innerHeight;
+
+//   if (boxPosition < windowHeight - 100) {
+//       box.classList.add('visible');
+//   }
+// });
+
